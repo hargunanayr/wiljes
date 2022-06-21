4d93fc539640a24f9bf33f5d947f34d26b156bd7
unknown <ryananugrah95@gmail.com>
2022-06-21 22:25:46 +0700

    path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages';
 import SigninPage from './pages/signin';
 
@@ -7,8 +7,8 @@ function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/" element={<Home />} exact />
-        <Route path="/sign-in" element={<SigninPage />} exact />
+        <Route path="/wiljes" element={<Home />} exact />
+        <Route path="/wiljes/sign-in" element={<SigninPage />} exact />
       </Routes>
     </Router>
   );
