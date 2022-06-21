82ac7a7ec21cfcf9f42f73777dee9a5868cbe677
unknown <ryananugrah95@gmail.com>
2022-06-21 23:15:27 +0700

    push github hashrouter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages';
 import SigninPage from './pages/signin';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <Routes>
-        <Route path="/wiljes" element={<Home />} exact />
-        <Route path="/wiljes/sign-in" element={<SigninPage />} exact />
+        <Route path="/" element={<Home />} exact />
+        <Route path="/sign-in" element={<SigninPage />} exact />
       </Routes>
     </Router>
   );
